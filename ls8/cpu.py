bfc4c8cf48f7307c3ba120a0cbb05b73c61f8353
Tashi Woeser <tashiwoeser@gmail.com>
2020-02-24 21:35:10 -0800

    day 1 mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,23 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+   #  Add list properties to the `CPU` class to hold 256 bytes of memory
+    # and 8 general-purpose registers.
+        self.reg = [0] * 8
+        self.pc = 0
+        self.ram = [0] * 8
+
+    # In `CPU`, add method `ram_read()` and `ram_write()`
+    # that access the RAM inside the `CPU` object.
+
+  # `ram_read()` should accept the address to read and return the value stored there.
+    def ram_read(self, memory_address_register):
+        value = self.ram[memory_address_register]
+        return value
+
+# `ram_write()` should accept a value to write, and the address to write it to.   
+    def ram_write(self, memory_data_register, memory_address_register):
+        self.ram[memory_address_register] = memory_data_register
 
     def load(self):
         """Load a program into memory."""
@@ -60,6 +76,44 @@ class CPU:
 
         print()
 
+        
+
     def run(self):
         """Run the CPU."""
-        pass
+
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+
+        running = True
+
+        while running:
+
+            IR = self.ram[self.pc]
+        # Using `ram_read()`, read the bytes at `PC+1` and `PC+2` from RAM into variables
+        # `operand_a` and `operand_b` in case the instruction needs them.
+        # operand_a = ram_read(IR + 1)
+        # operand_b = ram_read(IR + 2)
+
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+         # It needs to read the memory address that's stored in register `PC`,
+         # and store that result in `IR`, the _Instruction Register_.
+         # This can just be a local variable in `run()`.
+
+            if IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == HLT:
+                running = False
+        
+
+cpu = CPU()
+cpu.load()
+print(cpu.ram_read(0)) # test for ram_read works (print 130)
+cpu.ram_write(0b10000001, 0) # puts binary 129 in register 0
+print(cpu.ram_read(0)) # test for ram_write works (print 129)
