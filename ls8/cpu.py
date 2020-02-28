2abe6f53a0a158d8ba6a6c65bba2f695301e13cd
Tashi Woeser <tashiwoeser@gmail.com>
2020-02-27 20:14:03 -0800

    day3 n 4

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,63 +7,30 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-   #  Add list properties to the `CPU` class to hold 256 bytes of memory
-    # and 8 general-purpose registers.
-        self.reg = [0] * 8
-        self.pc = 0
-        self.ram = [0] * 256
+        self.ram = [0]*256
+        self.reg=[0]*8
+        self.pc=0
+        self.ir=0
+        self.SP=7
+        self.Flags = [0]*8
 
-    # In `CPU`, add method `ram_read()` and `ram_write()`
-    # that access the RAM inside the `CPU` object.
 
-  # `ram_read()` should accept the address to read and return the value stored there.
-    def ram_read(self, memory_address_register):
-        value = self.ram[memory_address_register]
-        return value
-
-# `ram_write()` should accept a value to write, and the address to write it to.   
-    def ram_write(self, memory_data_register, memory_address_register):
-        self.ram[memory_address_register] = memory_data_register
-
-    def load(self, filename):
+    def load(self):
         """Load a program into memory."""
         try:
             address = 0
-            # Open the file
             with open(sys.argv[1]) as f:
-                # Read all the lines
                 for line in f:
-                    # Parse out the comments
-                    comment_split = line.strip().split("#")
-                    # Cast number strings to ints
-                    value = comment_split[0].strip()
-                    # Ignore blank lines
-                    if value == "":
-                        continue
-                    instruction = int(value, 2)
-                    # Populate a memory array
-                    self.ram[address] = instruction
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    if num == "":
+                        continue  # Ignore blank lines ​
+                    value = int(num,2)   # Base 10, but ls-8 is base 2  
+                    self.ram[address] = value
                     address += 1
-
         except FileNotFoundError:
-            print("File not found")
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
             sys.exit(2)
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
 
 
     def alu(self, op, reg_a, reg_b):
@@ -94,45 +61,129 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
-
+    
+    def ram_read(self,MAR):
         
+        return self.ram[MAR]
+
+    def ram_write(self,MAR,MDR):
+        self.ram[MAR]=MDR
 
     def run(self):
         """Run the CPU."""
+        # for i in self.ram:
+        #     print(i)
+        Running=True
+        # print(chr(self.ram_read(self.pc)))
+        LDI=0b10000010
+        HLT=0b00000001
+        PRN=0b01000111 
+        MUL=0b10100010
+        PUSH=0b01000101
+        POP=0b01000110
+        CALL=0b01010000
+        RET=0b00010001
+        ADD=0b10100000
+        CMP=0b10100111
+        JMP=0b01010100
+        JEQ=0b01010101
+        JNE=0b01010110
+        L= 0
+        E= 0
+        G= 0
 
-        LDI = 0b10000010
-        PRN = 0b01000111
-        HLT = 0b00000001
+        while Running:
 
-        running = True
 
-        while running:
+            Command=self.ram_read(self.pc)
+            # print(Command)
+            if Command == LDI:
+                # print("LDI")
+                operand_a=self.ram_read(self.pc+1)
+                operand_b=self.ram_read(self.pc+2)
+                self.reg[operand_a]=operand_b
+                # print("LDI", self.reg)
+                self.pc+=3
+                # print(self.reg)
+            elif Command == HLT:
+                print("HLT")
+                Running=False
+                self.pc+=1
+            elif Command == PRN:
+                # print("PRN", self.reg)
+                reg = self.ram[self.pc + 1]
+                # self.reg=self.ram[self.pc+1]
+                print(self.reg[reg])
+                self.pc+=2
+            elif Command== MUL:
+                # print("MUL")
+                operand_a=self.ram_read(self.pc+1)
+                operand_b=self.ram_read(self.pc+2)
+                self.reg[operand_a]=self.reg[operand_a]*self.reg[operand_b]
+                self.pc+=3
+            elif Command==PUSH:
+                # print("PUSH")
+                reg=self.ram[self.pc+1]
+                val=self.reg[reg]
+                self.SP-=1
+                self.ram[self.reg[self.SP]]= val
+                self.pc+=2
+                
+            elif Command==POP:
+                # print("POP")
+                reg=self.ram[self.pc+1]
+                val=self.ram[self.reg[self.SP]]
+                self.reg[reg]=val
+                self.reg[self.SP] += 1
+                self.pc+=2
 
-            IR = self.ram[self.pc]
-        # Using `ram_read()`, read the bytes at `PC+1` and `PC+2` from RAM into variables
-        # `operand_a` and `operand_b` in case the instruction needs them.
-        # operand_a = ram_read(IR + 1)
-        # operand_b = ram_read(IR + 2)
+            elif Command == CALL:
+                val=self.pc+2
+                
+                reg=self.ram[self.pc+1]
+                subroutine_address=self.reg[reg]
+                self.reg[self.SP]-=1  
+                self.ram[self.reg[self.SP]]=val
 
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
+                self.pc=subroutine_address  
+            
+            elif Command ==ADD:
+                self.alu("ADD",self.ram_read(self.pc+1),self.ram_read(self.pc+2))
+                self.pc+=3
 
-         # It needs to read the memory address that's stored in register `PC`,
-         # and store that result in `IR`, the _Instruction Register_.
-         # This can just be a local variable in `run()`.
+            elif Command == RET:
+                return_address=self.reg[self.SP]
+                self.pc=self.ram[return_address]
 
-            if IR == LDI:
-                self.reg[operand_a] = operand_b
-                self.pc += 3
-            elif IR == PRN:
-                print(self.reg[operand_a])
-                self.pc += 2
-            elif IR == HLT:
-                running = False
-        
-
-cpu = CPU()
-# cpu.load()
-print(cpu.ram_read(0)) # test for ram_read works (print 130)
-cpu.ram_write(0b10000001, 0) # puts binary 129 in register 0
-print(cpu.ram_read(0)) # test for ram_write works (print 129)
+                self.reg[self.SP]+=1
+            elif Command==CMP:
+                self.Flags = CMP
+                if self.reg[self.ram_read(self.pc+1)] < self.reg[self.ram_read(self.pc+2)]:                  
+                    L=1
+                    G=0
+                    E=0
+                    self.Flags=CMP-0b00000100
+                elif self.reg[self.ram_read(self.pc+1)] > self.reg[self.ram_read(self.pc+2)]:
+                    G=1
+                    L=0
+                    E=0
+                    self.Flags=CMP-0b00000010
+                elif self.reg[self.ram_read(self.pc+1)] ==self.reg[self.ram_read(self.pc+2)]:
+                    E=1
+                    L=0
+                    G=0
+                    self.Flags=CMP-0b00000001
+                self.pc+=3
+                
+            elif Command==JMP:
+                self.pc = self.reg[self.ram_read(self.pc+1)]
+            elif Command==JEQ:
+                if E==1:
+                    self.pc = self.reg[self.ram_read(self.pc+1)]
+                else:
+                    self.pc+=2
+            elif Command==JNE:
+                if E ==0:
+                    self.pc = self.reg[self.ram_read(self.pc+1)] 
+                else:
+                    self.pc+=2
